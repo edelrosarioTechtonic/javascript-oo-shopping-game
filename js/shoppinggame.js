e0dd15d0576ddc8a15549b894494ff63f6540e5e
edelrosarioTechtonic <ervey.delrosario@techtonic.com>
2020-10-28 14:43:18 -0700

    module 4

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -271,8 +271,8 @@ const findPointsForExpDate = (prod) => {
 const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
     let pointsForExpDate = findPointsForExpDate(prod);
-
-    player.score = player.getCurrentScore() + pointsToBill + pointsForExpDate;
+    //player.addPoints(pointsForExpDate + pointsToBill);
+    player.score = player.score + pointsToBill + pointsForExpDate
     if(prod instanceof MagicProduct){
         if(prod.isBonus) player.addPoints(prod.points)
         else player.deductPoints(prod.points)
